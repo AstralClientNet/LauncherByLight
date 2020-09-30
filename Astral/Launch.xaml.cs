b356202d9e07106aa66f6fbddbde9630c08138d3
xFlqshlight <40709688+xFlqshlight@users.noreply.github.com>
2020-09-30 11:54:32 +0800

    New Launcher - now uses WPF instead of WinForms

---
+++ Astral/Launch.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace Pizzaria1
+{
+
+    /// <summary>
+    /// Interação lógica para UserControlInicio.xam
+    /// </summary>
+    public partial class UserControlInicio : UserControl
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress,
+            uint dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess,
+            IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+        // privileges
+        const int PROCESS_CREATE_THREAD = 0x0002;
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_READ = 0x0010;
+        // used for memory allocation
+        const uint MEM_COMMIT = 0x00001000;
+        const uint MEM_RESERVE = 0x00002000;
+        const uint PAGE_READWRITE = 4;
+        static bool alreadyAttemptedInject = false;
+        public static void versionFinderForLabel(string script, Label version)
+        {
+            using (PowerShell powerShell = PowerShell.Create())
+            {
+                powerShell.AddScript(script);
+                powerShell.AddCommand("Out-String");
+                Collection<PSObject> PSOutput = powerShell.Invoke();
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (PSObject pSObject in PSOutput)
+                    stringBuilder.AppendLine(pSObject.ToString());
+                version.Content = stringBuilder.ToString();
+            }
+        }
+        public static void InjectDLL(string DLLPath)
+        {
+            Process[] targetProcessIndex = Process.GetProcessesByName("Minecraft.Windows");
+            if (targetProcessIndex.Length > 0)
+            {
+                applyAppPackages(DLLPath);
+
+                Process targetProcess = Process.GetProcessesByName("Minecraft.Windows")[0];
+                IntPtr procHandle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ, false, targetProcess.Id);
+
+                IntPtr loadLibraryAddr = GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+
+                IntPtr allocMemAddress = VirtualAllocEx(procHandle, IntPtr.Zero, (uint)((DLLPath.Length + 1) * Marshal.SizeOf(typeof(char))), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+                UIntPtr bytesWritten;
+                WriteProcessMemory(procHandle, allocMemAddress, Encoding.Default.GetBytes(DLLPath), (uint)((DLLPath.Length + 1) * Marshal.SizeOf(typeof(char))), out bytesWritten);
+                CreateRemoteThread(procHandle, IntPtr.Zero, 0, loadLibraryAddr, allocMemAddress, 0, IntPtr.Zero);
+
+                alreadyAttemptedInject = false;
+            }
+
+            else
+            {
+                if (!alreadyAttemptedInject)
+                {
+                    alreadyAttemptedInject = true;
+                    Process.Start("minecraft://");
+                    //Thread.Sleep(1000);
+                    InjectDLL(DLLPath);
+                    MessageBox.Show("Injected!");
+                }
+                else
+                {
+                    MessageBox.Show("Make Sure Minecraft is open!");
+                    alreadyAttemptedInject = false;
+                }
+            }
+        }
+        public static void applyAppPackages(string DLLPath)
+        {
+            FileInfo InfoFile = new FileInfo(DLLPath);
+            FileSecurity fSecurity = InfoFile.GetAccessControl();
+            fSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier("S-1-15-2-1"), FileSystemRights.FullControl, InheritanceFlags.None, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            InfoFile.SetAccessControl(fSecurity);
+        }
+        public UserControlInicio()
+        {
+            InitializeComponent();
+            versionFinderForLabel("Get-AppPackage -name Microsoft.MinecraftUWP | select -expandproperty Version", mcVersion);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog FileIn = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> result = FileIn.ShowDialog();
+
+            if (result == true)
+            {
+                if (FileIn.SafeFileName.ToLower().EndsWith(".dll"))
+                {
+                    InjectDLL(FileIn.FileName);
+                }
+                else
+                {
+                    MessageBox.Show("You did not specify a DLL!");
+                }
+            }
+        }
+    }
+}
---
+++ Astral/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DiscordRPC;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace Pizzaria1
+{
+    public partial class MainWindow : Window
+    {
+        public DiscordRpcClient client;
+        string discordTime = "";
+        public static void versionFinderForLabel(string script, Label version)
+        {
+            using (PowerShell powerShell = PowerShell.Create())
+            {
+                powerShell.AddScript(script);
+                powerShell.AddCommand("Out-String");
+                Collection<PSObject> PSOutput = powerShell.Invoke();
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (PSObject pSObject in PSOutput)
+                    stringBuilder.AppendLine(pSObject.ToString());
+                version.Content = stringBuilder.ToString();
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            versionFinderForLabel("Get-AppPackage -name Microsoft.MinecraftUWP | select -expandproperty Version", discordRpc);
+            InitializeDiscordHome("Testing new app");
+        }
+        public void InitializeDiscordHome(string status)
+        {
+            int TimestampStart = 0;
+            int TimestampEnd = 0;
+            dynamic DateTimestampEnd = null;
+
+            if (discordTime != "" && Int32.TryParse(discordTime, out TimestampEnd))
+                DateTimestampEnd = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(TimestampEnd);
+
+            client = new DiscordRpcClient("722349140180205611");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Details = status,
+                State = "on " + discordRpc.Content,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logonewdiscord",
+                    LargeImageText = "Astral Launcher",
+                    SmallImageKey = "minecraft"
+                },
+                Timestamps = new Timestamps()
+                {
+                    Start = discordTime != "" && Int32.TryParse(discordTime, out TimestampStart) ? new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(TimestampStart) : DateTime.UtcNow,
+                    End = DateTimestampEnd
+                }
+
+            });
+        }
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+
+            switch (index)
+            {
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlInicio());
+                    break;
+                case 1:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new UserControlEscolha());
+                    break;
+                case 2:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new Update());
+                    break;
+                case 3:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new AboutUs());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TrainsitionigContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, (100 + (60 * index)), 0, 0);
+        }
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/AstralClientNet");
+        }
+    }
+}
